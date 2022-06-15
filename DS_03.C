fe5100afea6a25a6b303065a08ce08c1ec566737
JayBhogayata <jaybhogayata53@gmail.com>
2022-06-15 14:06:54 +0530

    prog 3

---
+++ DS_03.C
@@ -0,0 +1,44 @@
+// array del element
+#include<stdio.h>
+#include<conio.h>
+void main(){
+
+int LA[] = {1,3,5,7,8};
+int item = 10 , k = 3 , n = 5;
+int i = 0 , j = n;
+
+clrscr();
+
+printf("orignal array elements are : \n");
+
+
+for(i = 0 ; i < n  ; i++){
+
+printf("LA[%d] = %d \n", i , LA[i]);
+
+
+}
+
+j = k;
+
+while( j < n){
+	LA[j-1] = LA[j];
+	j = j + 1;
+
+}
+
+n = n - 1;
+
+printf("array after deleting the element \n")  ;
+
+for(i = 0 ; i < n  ; i++){
+
+printf("LA[%d] = %d \n", i , LA[i]);
+
+}
+
+getch();
+
+
+
+}
--- readMe.txt
+++ readMe.txt
@@ -1,2 +1,3 @@
 01 => printing array element
 02 => array element insert
+03 => delete array element
